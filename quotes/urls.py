fffc8aa91d887ef2f7d5b61c5a8e90b083fc250f
Evren Yaman <yamane@bu.edu>
2025-09-11 12:10:02 -0400

    fixed url paths

--- quotes/urls.py
+++ quotes/urls.py
@@ -8,8 +8,8 @@ from . import views
 
 #URL patterns specific to the quotes app:
 urlpatterns = [
-    path(r'/', views.home, name="home_page"),
-    path(r'/quote', views.quote, name="quote_page"),
+    path(r'', views.home, name="home_page"),
+    path(r'quote', views.quote, name="quote_page"),
     path(r'show_all', views.show_all, name="show_all_page"),
     path(r'about', views.about, name="about_page"),
 ]
