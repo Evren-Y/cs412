253419ff1d0ffe81c8137c1e5c4ae713461d2512
Evren Yaman <yamane@bu.edu>
2025-09-11 11:58:37 -0400

    Added comments and home page(same as quote page)

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,12 +1,15 @@
-# file: hw1/urls.py
+# file: quotes/urls.py
+# Author: Evren Yaman (yamane@bu.edu), 9/11/2025
+# Description: extension of cs412 urls.py file, creating the paths for all pages used in the quotes web application.
 
 from django.urls import path
 from django.conf import settings
 from . import views
 
-#URL patterns specific to the hw1 app:
+#URL patterns specific to the quotes app:
 urlpatterns = [
-    path(r'', views.quote, name="quote_page"),
+    path(r'/', views.home, name="home_page"),
+    path(r'/quote', views.quote, name="quote_page"),
     path(r'show_all', views.show_all, name="show_all_page"),
     path(r'about', views.about, name="about_page"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,6 @@
-# file: hw/view.py
+# file: quotes/views.py
+# Author: Evren Yaman (yamane@bu.edu), 9/11/2025
+# Description: This file creates requests for all three pages in the quotes/urls.py file, and it renders the pages with the necessary context.
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
@@ -6,6 +8,8 @@ import time
 import random
 
 # Create your views here.
+
+# python string list of Napoleon Quotes
 quotes = [
     "Never interrupt your enemy when he is making a mistake.",
     "I start out by believing the worst.",
@@ -13,6 +17,7 @@ quotes = [
     "The best way to keep one's word is not to give it.",
 ]
 
+# python string list of Napoleon image urls
 images = [
     "https://dreamsinparis.com/wp-content/uploads/2022/08/Painting-of-Napoleon-873x1024.jpg",
     "https://dreamsinparis.com/wp-content/uploads/2023/05/Napoleons-retreat-from-moscow-1024x780.jpg",
@@ -20,35 +25,68 @@ images = [
     "https://dreamsinparis.com/wp-content/uploads/2023/05/Napoleons-Return-from-Elba-1024x766.jpg",
 ]
 
+# Gets the current time
 time = time.ctime()
 
-def quote(request):
-    '''Function to respond to the "quote" request.'''
+def home(request):
+    '''Function to respond to the "home" request. Passes in a random quote string and image url string
+    to render from the html template page. Passes the time so that the footer in the base html file can work on every page.
+    Functionally the same as the quote page.
+    '''
 
     template_name = 'quotes/quote.html'
+    
+    # a rendered parameter that holds a random item from both the quotes and images python lists generated randomly
+    # passes time for the footer html
     context = {
         "quote": quotes[random.randint(1,4)-1],
         "image": images[random.randint(1,4)-1],
         "time": time,
     }
+
+    return render(request, template_name, context)
+
+def quote(request):
+    '''Function to respond to the "quote" request. Passes in a random quote string and image url string
+    to render from the html template page. Passes the time so that the footer in the base html file can work on every page.
+    '''
+
+    template_name = 'quotes/quote.html'
+    
+    # a rendered parameter that holds a random item from both the quotes and images python lists generated randomly
+    # passes time for the footer html
+    context = {
+        "quote": quotes[random.randint(1,4)-1],
+        "image": images[random.randint(1,4)-1],
+        "time": time,
+    }
+
     return render(request, template_name, context)
 
 def show_all(request):
-    '''Function to respond to the "show_all" request.'''
+    '''Function to respond to the "show_all" request. Passes in both the python lists(quotes and images)
+    to render from the html template page. Passes the time so that the footer in the base html file can work on every page.
+    '''
 
     template_name = 'quotes/show_all.html'
+
+    # a rendered parameter that holds both the quotes and images python lists; passes time for the footer html
     context = {
         "quotes": quotes,
         "images": images,
         "time": time,
     }
+
     return render(request, template_name, context)
 
 def about(request):
-    '''Function to respond to the "about" request.'''
+    '''Function to respond to the "about" request. Passes the time so that the footer in the base html file can work on every page.'''
 
     template_name = 'quotes/about.html'
+
+    # passes the time variable for the footer html
     context = {
         "time": time,
     }
+
     return render(request, template_name, context)
