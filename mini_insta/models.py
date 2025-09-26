fa185abf4fb14d46c9f730113ea3389d311d55e3
Evren Yaman <yamane@bu.edu>
2025-09-26 16:36:14 -0400

    Created Profile model for mini_insta

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    '''Encapsulate the data of a blog Article by an author'''
+
+    username = models.TextField(blank=True)
+    display_name = models.TextField(blank=True)
+    bio_text = models.TextField(blank=True)
+    join_date = models.DateTimeField(auto_now=True)
+    profile_image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'{self.bio_text} by {self.display_name}'
