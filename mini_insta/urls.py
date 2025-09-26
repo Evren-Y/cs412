31c9de56bb34f68b96497ffeadb441e9f9a134bf
Evren Yaman <yamane@bu.edu>
2025-09-26 18:03:32 -0400

    templates created

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,5 +3,5 @@ from .views import ProfileListView, ProfileDetailView
 
 urlpatterns = [
     path('', ProfileListView.as_view(), name="show_all_profiles"),
-    path('profile/<int:pk>', ProfileDetailView.as_view(), name='profile'),
+    path('profile/<int:pk>/', ProfileDetailView.as_view(), name='profile'),
 ]
